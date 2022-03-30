8eb04026659c83958a3b138c9150ec436b30a0db
Pedro Evaristo Gonzalez Sanchez <pedro.gonzalez@pesol.es>
2022-03-30 15:41:26 -0500

    [IMP] web_chatt_filtering: black, isort, prettier

--- web_chatt_filtering/static/src/js/chatter.js
+++ web_chatt_filtering/static/src/js/chatter.js
@@ -1,14 +1,14 @@
 /** Copyright <2019> Pesol <pedro.gonzalez@pesol.es>
  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define('mymail.Chatter', function (require) {
+odoo.define("mymail.Chatter", function (require) {
     "use strict";
 
-    var Chatt = require('mail.Chatter');
+    var Chatt = require("mail.Chatter");
 
     Chatt.include({
         events: _.extend({}, Chatt.prototype.events, {
-            'keyup input#search_partner': function (ev) {
+            "keyup input#search_partner": function (ev) {
                 if (ev.which === $.ui.keyCode.ENTER) {
                     this.filter_message($(ev.target).val().trim());
                 }
@@ -19,39 +19,45 @@ odoo.define('mymail.Chatter', function (require) {
             var self = this;
             var dThread = this.fields.thread;
             this._rpc({
-                model: 'mail.message',
-                method: 'message_fetch',
+                model: "mail.message",
+                method: "message_fetch",
                 args: [domain],
                 kwargs: {limit: this.original_msgs.length},
-            })
-                .then(function (msgs) {
-                    // Add messages to the thread in case not empty
-                    if (msgs.length > 0) {
-                        _.each(msgs, function (messageData) {
-                            if (self.reverse_author) {
-                                if (!messageData.author_id[1].toLowerCase().includes(
-                                    self.substring.substr(1))) {
-                                    self.call('mail_service', 'addMessage',
-                                        messageData, {silent: true});
-                                }
-                            } else {
-                                self.call('mail_service', 'addMessage',
-                                    messageData, {silent: true});
+            }).then(function (msgs) {
+                // Add messages to the thread in case not empty
+                if (msgs.length > 0) {
+                    _.each(msgs, function (messageData) {
+                        if (self.reverse_author) {
+                            if (
+                                !messageData.author_id[1]
+                                    .toLowerCase()
+                                    .includes(self.substring.substr(1))
+                            ) {
+                                self.call("mail_service", "addMessage", messageData, {
+                                    silent: true,
+                                });
                             }
-                        });
-                        // Reload thread view
-                        dThread._threadWidget.render(dThread._documentThread, {});
-                        self.$('#search_partner').val(self.substring);
-                    } else {
-                        self.$('#search_partner').val("No messages found");
-                    }
-                    self.$('#search_partner').select();
-                });
+                        } else {
+                            self.call("mail_service", "addMessage", messageData, {
+                                silent: true,
+                            });
+                        }
+                    });
+                    // Reload thread view
+                    dThread._threadWidget.render(dThread._documentThread, {});
+                    self.$("#search_partner").val(self.substring);
+                } else {
+                    self.$("#search_partner").val("No messages found");
+                }
+                self.$("#search_partner").select();
+            });
         },
 
         filter_message: function (substring) {
-            if (this.last_model_id !== this.fields.thread.model_id ||
-            this.last_res_id !== this.fields.thread.res_id) {
+            if (
+                this.last_model_id !== this.fields.thread.model_id ||
+                this.last_res_id !== this.fields.thread.res_id
+            ) {
                 this.last_model_id = this.fields.thread.model_id;
                 this.last_res_id = this.fields.thread.res_id;
                 this.original_msgs = Array.from(this.fields.thread.value.res_ids);
@@ -68,9 +74,14 @@ odoo.define('mymail.Chatter', function (require) {
             // Reset messages to none
             this.fields.thread._documentThread._messages = [];
             // Get domain
-            var domain = ['&', ["id", "in", this.original_msgs], '|',
-                ['body', 'ilike', substring], ['author_id', 'ilike', substring]];
-            if (substring.trim().charAt(0) === '-') {
+            var domain = [
+                "&",
+                ["id", "in", this.original_msgs],
+                "|",
+                ["body", "ilike", substring],
+                ["author_id", "ilike", substring],
+            ];
+            if (substring.trim().charAt(0) === "-") {
                 domain = [["id", "in", this.original_msgs]];
                 this.reverse_author = true;
             }
